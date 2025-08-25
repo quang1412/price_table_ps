1a559b9cd6949409ee950e3d91eeb4ff0dafbc71
QuangPlus <trinhdacquang@gmail.com>
2025-08-26 02:00:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,64 @@
-const readSheet2json = (url) => { 
+(function(){
+
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString); 
+  
+  const readSheetTable = (url) => {  
     return new Promise((resolve, reject) => {
-        var txt = window.fetch(url).then(r => {
-            if (!r.ok) {
-                throw new Error(`HTTP error! status: ${r.status}`);
-            } 
-            return r.text();
-        }).then(txt => {
-            var jsonString = txt.slice(47, -2);
-            return(jsonString);
-        }).then(jsonStr => {
-            let json = [];
-            let lines = jsonStr.split("\n");
-            lines.forEach(line => {
-                let row = [];
-                    line.split(',').forEach(col => {
-                        row.push(col);
-                    })
-                json.push(row);
-                //console.log(row);
-            })
-            return resolve(json) 
-            
-        });
-        
+      window.fetch(url).then(r => {
+        if (!r.ok) {
+          throw new Error(`HTTP error! status: ${r.status}`);
+        } 
+        return r.text();
+      }).then(html => {
+        return resolve(html);
+      });
     });
-} 
-
-
-async function generateHtmlTable(data, target) {
-    if(!data) return;
-  let htmlTable = '<table id="myTable" class="pure-table pure-table-bordered">'; 
-  htmlTable += '<tbody>'; 
-  for (let row = 0; row < data.length; row++) { 
-    let isEmptyRow = data[row][0] == '-'; 
-    if(data[row][2] == ''){
-      break;
-    }
-    if(isEmptyRow){
-      htmlTable += '<tr class="emptyRow"><td colspan="8"></td></tr>';
-    }
-    else{
-      htmlTable += '</tr>';
-        data[row].forEach((value, index) => {
-            htmlTable += '<td>' + value.replaceAll('"', '') + '</td>';
-        });
-      htmlTable += '</tr>';
-    }
-  }
-  htmlTable += '</tbody>';
-  htmlTable += '</table>';
-    document.queryselector("target")?.innerHTML = htmlTable
-  return htmlTable;
-}
-
-$(document).ready(async function(){
-    let id = '1B0lsfTAz0T2YL2-J5D3ufloYwqlJeZbdqxn06VRbTno';
-    let gid = '897983425';
-    let range = 'C1:J';
+  } 
+  
+  $(document).ready(async function(){ 
+    let sid = '1B0lsfTAz0T2YL2-J5D3ufloYwqlJeZbdqxn06VRbTno'; 
+    let gid = urlParams.get('gid')
+    let range = 'A:H';
     let headers = 0
-    let url = `https://docs.google.com/spreadsheets/d/${id}/gviz/tq?tqx=out:csv&tq&gid=${gid}&range=${range}&headers=${headers}`;
-    
-    let data = await readSheet2json();
-    generateHtmlTable(data, 'div#col1');
-    })
+    let url = `https://docs.google.com/spreadsheets/d/${sid}/gviz/tq?tqx=out:html&tq&gid=${gid}&range=${range}&headers=${headers}`;
+
+    let htmlTable1 = window.location.protocol == 'file:' ? testdata() : await readSheetTable(url);
+    document.querySelector('div#col1').innerHTML = htmlTable1;
+
+    let htmlTable2 = window.location.protocol == 'file:' ? testdata() : await readSheetTable(url);
+    document.querySelector('div#col2').innerHTML = htmlTable2;
+
+    mergeCols();
+  });
+
+  function mergeCols(){
+    let lastTd;
+    let rowSpan = 1;
+    $('table tr td:first-child').each((i, td) => {
+      let tr = $(td).closest('tr')[0]
+      let text = td.textContent;
+      if(text == '-') tr.classList.add('emptyRow');
+      if(text == '\u00a0'){
+        rowSpan += 1; lastTd.setAttribute('rowspan', rowSpan); $(td).hide();
+      } else{
+        rowSpan = 1; lastTd = td;
+      }
+    });
+
+    let lastTd2;
+    let rowSpan2 = 1;
+    $('table tr td:nth-child(2)').each((i, td) => {
+        let tr = $(td).closest('tr')[0]
+        let text = td.textContent;
+       if(text == '\u00a0'){
+           rowSpan2 += 1; lastTd2.setAttribute('rowspan', rowSpan2);
+           $(td).hide();
+       } else{
+           rowSpan2 = 1; lastTd2 = td;
+       }
+    }); 
+
+    $(document.body).show();
+  }
+})();
