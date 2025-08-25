c642892c1e64f984d6e5008edbf083e0cae890cb
QuangPlus <trinhdacquang@gmail.com>
2025-08-25 21:02:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-const readSheet2json = () => {
-    var id = '1B0lsfTAz0T2YL2-J5D3ufloYwqlJeZbdqxn06VRbTno';
-    var gid = '897983425';
-    let range = 'C1:J';
-    let headers = 0
-    var url = `https://docs.google.com/spreadsheets/d/${id}/gviz/tq?tqx=out:csv&tq&gid=${gid}&range=${range}&headers=${headers}`;
-    
+const readSheet2json = (url) => { 
     return new Promise((resolve, reject) => {
         var txt = window.fetch(url).then(r => {
             if (!r.ok) {
@@ -33,8 +27,8 @@ const readSheet2json = () => {
 } 
 
 
-async function generateHtmlTable(x = "C3:J") {
-  const data = await readSheet2json()
+async function generateHtmlTable(data, target) {
+    if(!data) return;
   let htmlTable = '<table id="myTable" class="pure-table pure-table-bordered">'; 
   htmlTable += '<tbody>'; 
   for (let row = 0; row < data.length; row++) { 
@@ -55,8 +49,17 @@ async function generateHtmlTable(x = "C3:J") {
   }
   htmlTable += '</tbody>';
   htmlTable += '</table>';
-document.body.innerHTML = htmlTable
+    document.queryselector("target")?.innerHTML = htmlTable
   return htmlTable;
 }
 
-generateHtmlTable()
+$(document).ready(async function(){
+    let id = '1B0lsfTAz0T2YL2-J5D3ufloYwqlJeZbdqxn06VRbTno';
+    let gid = '897983425';
+    let range = 'C1:J';
+    let headers = 0
+    let url = `https://docs.google.com/spreadsheets/d/${id}/gviz/tq?tqx=out:csv&tq&gid=${gid}&range=${range}&headers=${headers}`;
+    
+    let data = await readSheet2json();
+    generateHtmlTable(data, 'div#col1');
+    })
