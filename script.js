b1e987892e8a5305ed22738bcb2b2ef81861b54c
QuangPlus <trinhdacquang@gmail.com>
2025-08-29 01:02:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 (function() {
-
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
 
@@ -16,31 +15,31 @@
     });
   }
 
-	const setTime = function(){
-		const today = new Date(); 
-		const formattedDateVN = today.toLocaleDateString('vi-VN');
-		console.log(formattedDateVN); 
-		$('span#date')?.text(formattedDateVN);
-	
-	}
+  const setTime = function() {
+    const today = new Date();
+    const formattedDateVN = today.toLocaleDateString('vi-VN');
+    console.log(formattedDateVN);
+    $('span#date')?.text(formattedDateVN);
+
+  }
   $(document).ready(async function() {
-	setTime();
+    setTime();
     let sid = '1B0lsfTAz0T2YL2-J5D3ufloYwqlJeZbdqxn06VRbTno';
     let gid = urlParams.get('gid') || 1074479198
     let headers = 0
-   
-		let range = 'A2:H';
+
+    let range = 'A2:H';
     let url = `https://docs.google.com/spreadsheets/d/${sid}/gviz/tq?tqx=out:html&tq&gid=${gid}&range=${range}&headers=${headers}`;
     let htmlTable1 = window.location.protocol == 'file:' ? testdata() : await readSheetTable(url);
-		
+
     let range2 = 'J2:Q';
     let url2 = `https://docs.google.com/spreadsheets/d/${sid}/gviz/tq?tqx=out:html&tq&gid=${gid}&range=${range2}&headers=${headers}`;
-		let htmlTable2 = window.location.protocol == 'file:' ? testdata() : await readSheetTable(url2);
-   
+    let htmlTable2 = window.location.protocol == 'file:' ? testdata() : await readSheetTable(url2);
+
     document.querySelector('div#col1').innerHTML = htmlTable1;
-		document.querySelector('div#col2').innerHTML = htmlTable2;
-	
-    mergeCols();
+    document.querySelector('div#col2').innerHTML = htmlTable2;
+
+    mergeCols(); 
   });
 
   function mergeCols() {
@@ -80,8 +79,10 @@
       if (text !== '\u00a0') {
         td.innerHTML = `<div>${text}</div>`;
       }
-    });
-
-    // $(document.body).show();
-  }
+    }); 
+  } 
 })();
+
+function darkModeSwitch(e){
+	$('body').toggleClass('darkMode', this.checked);
+}
